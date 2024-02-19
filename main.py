d2a17fd159ed490737c5a399a4ff8350a54efbc2
DataAce-yousaf <20pwbcs0781@uetpeshawar.edu.pk>
2024-02-19 16:34:49 +0500

    created main.py file

---
+++ main.py
@@ -0,0 +1,11 @@
+#write python function to calculate fibonachi
+def fibonachi(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonachi(n-1) + fibonachi(n-2)
+
+#use the function
+print(fibonachi(10))
